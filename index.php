b3c7dd22f6685f2f2b2ca4fbaf63341e539758ec
srdjanpetkovski <srdjanpetkovski@gmail.com>
2021-11-28 20:41:37 +0100

    topBar css

--- index.php
+++ index.php
@@ -6,9 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <link rel="stylesheet" href="scss/style.css" >
-    <title>Law office</title>
+    <title>Law Office</title>
 </head>
 <body>
-    <h1>welcome to law office online</h1>
+
+    <!-- top bar -->
+    <section class="topBar container">
+        <a href="" class="logo">Law <span>Office</span></a>
+        <ul>
+            <li><a><i class="fab fa-facebook-f"></i></a></li>
+            <li><a><i class="fab fa-twitter"></i></a></li>
+            <li><a><i class="fab fa-instagram"></i></a></li>
+        </ul>
+    </section>
+
 </body>
 </html>
