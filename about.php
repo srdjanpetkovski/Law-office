e1b6b2b42a444d0984e7e997dc9dc3905fbeab8e
srdjanpetkovski <srdjanpetkovski@gmail.com>
2021-11-30 21:06:46 +0100

    team css

--- about.php
+++ about.php
@@ -1,5 +1,37 @@
 <?php include "partials/header.php"; ?>
 
-    <h1>About</h1>
+    <!-- team -->
+    <section class="team">
+        <div class="container">
+            <article>
+                <img src="img/people1.jpg" alt="">
+                <h3>Jessica Moore</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+                <button>Learn more</button>
+                <button>Contact</button>
+            </article>
+            <article>
+            <img src="img/people2.jpg" alt="">
+                <h3>Anthony Davis</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+                <button>Learn more</button>
+                <button>Contact</button>
+            </article>
+            <article>
+            <img src="img/people3.jpg" alt="">
+                <h3>Anna Blue</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+                <button>Learn more</button>
+                <button>Contact</button>
+            </article>
+            <article>
+            <img src="img/people4.jpg" alt="">
+                <h3>Michael Hopkins</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+                <button>Learn more</button>
+                <button>Contact</button>
+            </article>
+        </div>
+    </section>
 
 <?php include "partials/footer.php"; ?>
